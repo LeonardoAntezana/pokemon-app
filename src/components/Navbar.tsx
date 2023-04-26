4f0c68b28db9f5d740a85c92f3e504c5aa8fd3a0
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-26 20:01:09 -0300

    creando componente Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import '../sass/_components/navbar.scss'
+import { NavLink } from 'react-router-dom';
+import { BsFillStarFill } from 'react-icons/bs'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <span className='navbar__title'>POKEMON API</span>
+      <div className='navbar__right'>
+        <NavLink to='/favorites' className='navbar__link'>
+          {({isActive}) => <BsFillStarFill color={isActive ? 'white' : '#856f6f'} size={20}/>}
+        </NavLink>
+      </div>
     </div>
   )
 }
