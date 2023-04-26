8dbcdd004c895b7c4292636c39656ecb3be7282d
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-26 15:01:04 -0300

    creando navegacion base

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components'
+import './app.scss'
+
+import Home from './pages/Home'
 
 function App() {
   return (
-    <>
-  
-    </>
+    <div className='app'>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' Component={Home}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
