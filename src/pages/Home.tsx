7685e5811d92d2c0e2790ec1d154aa8908393b0d
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-27 14:09:16 -0300

    implementando logica y estilos de vista Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,48 @@
+import { ChangeEvent, useState, useEffect } from "react"
+import { resolvePokemon } from "../utilities/resolvePokemon"
+import { createPokemon } from "../utilities/createPokemon"
+import { BASE_URL } from "../constants/endPoints"
+import { PokemonPromise } from "../models/pokemon.promise"
+import Pokemon from "../models/pokemon"
+import { InputCustom } from "../components"
+import '../sass/_pages/Home.scss'
 
 const Home = () => {
+
+  const [request, setRequest] = useState<string>(BASE_URL)
+  const [inputValue, setInputValue] = useState<string>('')
+  const [pokemons, setPokemons] = useState<Pokemon[]>()
+  
+  const onHandleChange = (event : ChangeEvent<HTMLInputElement>) => setInputValue(event.target.value)
+
+  const transformData = async () => {
+    const data = await fetch(BASE_URL + 'pokemon').then(res => res.json());
+    const promises: PokemonPromise[] = data.results.map( async (poke: PokemonPromise) => resolvePokemon(poke));
+    const dataResolve = await Promise.all(promises);
+    const newPokemons = dataResolve.map(pokemon => createPokemon(pokemon));
+    setPokemons(newPokemons);
+  }
+
+  useEffect(() => {
+    transformData();
+  }, [])
+
   return (
-    <div>
-      Home
+   <div className='home'>
+     <div className='filters'>
+      <select className='home__select'>
+        <option value="None">Ninguno</option>
+        <option value="Planta">Planta</option>
+        <option value="Fuego">Fuego</option>
+      </select>
+      <InputCustom 
+      onChange={onHandleChange}
+      className='input__search'
+      placeholder='Buscar pokemon...'
+      value={inputValue}
+      />
     </div>
+   </div>
   )
 }
 
