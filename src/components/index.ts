6fa45354c613ec661222aeddc33ce04004edd5f4
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-29 13:26:36 -0300

    update barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { default as Carousel } from './Carousel';
 export { default as InputCustom } from './InputCustom';
 export { default as Navbar } from './Navbar';
 export { default as PokemonCard } from './PokemonCard';
+export { default as Title } from './Title';
