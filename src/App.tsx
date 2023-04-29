a1938c9b2309f214d51b64466f8d30910d575c73
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-29 13:26:58 -0300

    importando vista Details a la navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from './components'
 import './app.scss'
 
 import Home from './pages/Home'
+import Details from './pages/Details'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' Component={Home}/>
+          <Route path='/details/:idPokemon' Component={Details}/>
         </Routes>
       </BrowserRouter>
     </div>
