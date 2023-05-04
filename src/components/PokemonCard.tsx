d2b72c0f4651ba11bdc9d550c3291f2a71199ae1
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-04 12:46:18 -0300

    cambiando estilos de componentes reutilizables

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -6,15 +6,13 @@ import '../sass/_components/pokemonCard.scss'
 interface Props {
   pokemon: Pokemon,
   className?: string,
-  onClick?: (id:number) => void,
 }
 
-const PokemonCard: FC<Props> = ({ pokemon, className, onClick }) => {
-  const { id, name, images, types } = pokemon;
+const PokemonCard: FC<Props> = ({ pokemon, className }) => {
+  const { name, images, types } = pokemon;
   return (
     <div
-    className={`pokemonCard${className ? ` ${className}`: ''}`}  
-    onClick={() => onClick && onClick(id)}>
+    className={`pokemonCard${className ? ` ${className}`: ''}`}>
       <Title>{name}</Title>
       <img className='image' src={`${images[1]}`}/>
       <div className='pokemonCard__types'>
