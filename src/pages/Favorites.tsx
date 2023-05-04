453c3a3c6a6abbb9f38ccdcebd2ed05f9eca6d2d
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-04 12:47:07 -0300

    creando vista Favorites y estilos demas vistas

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,20 @@
 import { useAppSelector } from "../redux/hooks";
+import { Link } from "react-router-dom";
+import { PokemonCard } from "../components";
+import '../sass/_pages/Favorites.scss'
 
 const Favorites = () => {
-  const favorites = useAppSelector(state => state.favorites.value) 
-  console.log(favorites.length)
-  return(
-    <div>
-      FAVORITES
+  const favorites = useAppSelector(state => state.favorites.value)
+
+  return (
+    <div className='favorites__screen'>
+      <h1 className='favorites__screen__title'>Favoritos</h1>
+      <div className='favorites__container'>
+        {favorites.map(pokemon => 
+        <Link to={`/details/${pokemon.id}`} key={pokemon.id} state={{pokemon}}>
+          <PokemonCard pokemon={pokemon}/>
+        </Link>)}
+      </div>
     </div>
   )
 }
