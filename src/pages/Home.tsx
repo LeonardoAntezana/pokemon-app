f7376cc8ae99b09dbd01f0685db7ea899083ec6c
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-04 13:55:13 -0300

    agregando estado y cambio de select

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState, useEffect } from "react"
+import { ChangeEvent, useState, useEffect, useRef, FormEventHandler } from "react"
 import { resolvePokemon } from "../utilities/resolvePokemon"
 import { createPokemon } from "../utilities/createPokemon"
 import { BASE_URL } from "../constants/endPoints"
@@ -11,52 +11,56 @@ import { Link } from "react-router-dom"
 const Home = () => {
 
   const [request, setRequest] = useState<string>('pokemon')
-  const [inputValue, setInputValue] = useState<string>('')
   const [pokemons, setPokemons] = useState<Pokemon[]>()
-  
+  const inputValue = useRef<string>();
+
   const transformData = async () => {
     const data = await fetch(BASE_URL + request).then(res => res.json());
-    const promises: PokemonPromise[] = data.results.map( async (poke: PokemonPromise) => resolvePokemon(poke));
+    const promises: PokemonPromise[] = data.results.map(async (poke: PokemonPromise) => resolvePokemon(poke));
     const dataResolve = await Promise.all(promises);
     const newPokemons = dataResolve.map(pokemon => createPokemon(pokemon));
     setPokemons(newPokemons);
   }
-  
+
   useEffect(() => {
     transformData();
-  }, [])
-  
-  const onHandleChange = (event : ChangeEvent<HTMLInputElement>) => setInputValue(event.target.value)
+  }, [request])
+
+  const onHandleChange = (event: ChangeEvent<HTMLInputElement>) => inputValue.current = event.target.value;
+
+  const onHandleSelect = (event: ChangeEvent<HTMLSelectElement>) => {}
 
   return (
-   <div className='home'>
-     <div className='filters'>
-      <select className='home__select'>
-        <option value="None">Ninguno</option>
-        <option value="Planta">Planta</option>
-        <option value="Fuego">Fuego</option>
-      </select>
-      <InputCustom 
-      onChange={onHandleChange}
-      className='input__search'
-      placeholder='Buscar pokemon...'
-      value={inputValue}
-      />
+    <div className='home'>
+      <div className='filters'>
+        <select
+          onChange={onHandleSelect}
+          className='home__select'>
+          <option value="Ninguno">Ninguno</option>
+          <option value="Planta">Planta</option>
+          <option value="Fuego">Fuego</option>
+        </select>
+        <InputCustom
+          onChange={onHandleChange}
+          className='input__search'
+          placeholder='Buscar pokemon...'
+          value={inputValue.current}
+        />
+      </div>
+      <div className='container__pokemons'>
+        {pokemons?.length !== 0 && pokemons?.map((poke: Pokemon) => {
+          return (
+            <Link
+              to={`/details/${poke.id}`}
+              state={{ pokemon: poke }}
+              key={poke.id}
+              className='card'>
+              <PokemonCard pokemon={poke} className='card' />
+            </Link>
+          )
+        })}
+      </div>
     </div>
-    <div className='container__pokemons'>
-      {pokemons?.length !== 0 && pokemons?.map((poke: Pokemon) => {
-        return(
-          <Link 
-          to={`/details/${poke.id}`}
-          state={{pokemon : poke}} 
-          key={poke.id} 
-          className='card'>
-            <PokemonCard pokemon={poke} className='card'/>
-          </Link>
-        )
-      })}
-    </div>
-   </div>
   )
 }
 
