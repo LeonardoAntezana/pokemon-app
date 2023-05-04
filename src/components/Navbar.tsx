6f50dd1d0294fcb0af43d5ca980f9d6bfc877a5a
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-04 10:34:10 -0300

    agregando ruta a Home en el navBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,12 @@ import '../sass/_components/navbar.scss'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <span className='navbar__title'>POKEMON API</span>
+      <NavLink to='/'>
+        <h1 className='navbar__title'>POKEMON API</h1>
+      </NavLink>
       <div className='navbar__right'>
         <NavLink to='/favorites' className='navbar__link'>
-          {({isActive}) => <BsFillStarFill color={isActive ? 'white' : '#856f6f'} size={20}/>}
+          {({ isActive }) => <BsFillStarFill color={isActive ? 'white' : '#856f6f'} size={20} />}
         </NavLink>
       </div>
     </div>
