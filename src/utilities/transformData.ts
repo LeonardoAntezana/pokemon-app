29fd68615b06348f3a12f789e6610edc32d49f47
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-11 10:49:04 -0300

    comentando

--- src/utilities/transformData.ts
+++ src/utilities/transformData.ts
@@ -4,7 +4,7 @@ import { createPokemon } from ".";
 
 export const transformData = async (pokemons : PokemonPromise[]) => {
   const promises = pokemons.map(async (poke: PokemonPromise) => resolvePokemon(poke));
-  const dataResolve = await Promise.all(promises);
-  const newPokemons = dataResolve.map(pokemon => createPokemon(pokemon));
+  const dataResolve = await Promise.all(promises); // obtenemos data de cada pokemon y la resolvemos
+  const newPokemons = dataResolve.map(pokemon => createPokemon(pokemon)); // modelamos la informacion de cada objeto Pokemon
   return newPokemons;
 }
