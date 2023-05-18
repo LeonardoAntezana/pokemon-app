7881a83495e8bb09a80f3e17d0581c3c37592512
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-18 13:25:33 -0300

    modificando estilos de vista Details(responsive)

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,13 +1,15 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../redux/hooks";
 import { addFavorite, deleteFavorite } from "../redux/slices/favoritesSlice";
+import { CapitalizeWord, selectBackgroundColor } from "../utilities";
 import { Title, Carousel, Stat, ButtonCustom } from "../components";
 import { StatPokemon } from "../models/pokemon.stat";
 import { AiTwotoneStar } from 'react-icons/ai'
 import '../sass/_pages/Details.scss'
 
 const Details = () => {
-  
+
   const dispatch = useAppDispatch();
   const favorites = useAppSelector(state => state.favorites.value);
 
@@ -21,19 +23,30 @@ const Details = () => {
 
   const onHandleFav = () => pokemonInState() ? dispatch(deleteFavorite(id)) : dispatch(addFavorite(pokemon))
 
+  useEffect(() => {
+    document.title = `Pokemon - ${CapitalizeWord(name)}`
+  }, [])
+
   return (
     <div className='container'>
       <div className='box'>
         <Carousel images={filterImages} className='carousel'/>
         <div className='info'>
           <Title className='name'>{name}</Title>
-          <div>
+          <div className='info__container'>
             <p>Experience base: {base_experience}</p>
             <p>Weight: {weight}</p>
-            <p>Types:</p>
-            {types.map((type: any, index: number) => <span key={index}>{type}</span>)}
             <p>Abilities:</p>
-            {abilities.map((ab: any, index: number) => <p key={index}>{ab}</p>)}
+            <div className='container__abilities'>
+            {abilities.map((ab: any, index: number) => <span className='abilitie' key={index}>{ab}</span>)}
+            </div>
+            <p>Types:</p>
+            <div className='container__types'>
+            {types.map((type: any, index: number) => {
+              const color = selectBackgroundColor(type);
+              return <span className='type' style={{backgroundColor: color}} key={index}>{type}</span>
+            })}
+            </div>
           </div>
         </div>
         <div className='container__stats'>
